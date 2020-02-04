6174a274bc6419d3e725023ea8edfe1c1bc2f7e3
Veseha <sveula@list.ru>
2020-02-04 20:48:08 +0600

    Presintation

--- create_tile_images.py
+++
@@ -1,6 +0,0 @@
-a = {}
-
-for i in range(20):
-    b = input()
-    a['wall' + b] = "load_image(wall{}.png)".format(b)
-print(a)
--- main3.py
+++
@@ -1,8 +0,0 @@
-import pygame
-start_ticks = pygame.time.get_ticks() #starter tick
-seconds = (pygame.time.get_ticks() - start_ticks)  # calculate how many seconds
-
-while True:    # mainloop
-    if seconds > 10:    # if more than 10 seconds close the game
-        break
-    print(start_ticks) # print how many seconds
